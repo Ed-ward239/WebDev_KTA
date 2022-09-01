9558018dac499d233b9b95faccb1ba891a7a4de8
Edward Lee <93949554+Ed-ward239@users.noreply.github.com>
2022-08-31 23:34:42 -0400

    new commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KTAWebapp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
